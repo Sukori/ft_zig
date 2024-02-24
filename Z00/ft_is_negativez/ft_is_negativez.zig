9dfa14d2f4a13c9ccf4346dd9c7cf8a20d066e21
Philippe Berset <pberset@student.42lausanne.ch>
2024-02-24 20:42:46 +0100

    negative

--- Z00/ft_is_negativez/ft_is_negativez.zig
+++ Z00/ft_is_negativez/ft_is_negativez.zig
@@ -1,18 +1,16 @@
 const std = @import("std");
 
-pub fn ft_print_numberz() !void {
+pub fn ft_is_negativez(num: i64) !void {
     const stdout_file = std.io.getStdOut().writer();
     var bw = std.io.bufferedWriter(stdout_file);
     const stdout = bw.writer();
-    var num: u8 = '0';
 
-    while (num <= '9') {
-        try stdout.print("{c}", .{num});
-        num += 1;
-    }
+    if (num < 0) {
+        try stdout.print("N", .{});
+    } else try stdout.print("P", .{});
     try bw.flush();
 }
 
 pub fn main() !void {
-    try ft_print_numberz();
+    try ft_is_negativez(-8);
 }
--- Z00/ft_print_alphabetz/src/ft_print_alphabetz.zig
+++ Z00/ft_print_alphabetz/src/ft_print_alphabetz.zig
@@ -1,18 +1,18 @@
 const std = @import("std");
 
-pub fn ft_print_numberz() !void {
+pub fn ft_print_alphabetz() !void {
     const stdout_file = std.io.getStdOut().writer();
     var bw = std.io.bufferedWriter(stdout_file);
     const stdout = bw.writer();
-    var num: u8 = '0';
+    var alpha: u8 = 'a';
 
-    while (num <= '9') {
-        try stdout.print("{c}", .{num});
-        num += 1;
+    while (alpha <= 'z') {
+        try stdout.print("{c}", .{alpha});
+        alpha += 1;
     }
     try bw.flush();
 }
 
 pub fn main() !void {
-    try ft_print_numberz();
+    try ft_print_alphabetz();
 }
